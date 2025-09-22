499893a561b9c175a1b5d8bec6fe76e4d3955505
kavinduwimalaratne <kavinduw24@gmail.com>
2025-09-22 18:05:06 -0500

    Built basic map. Can expand soon

---
+++ airport_escape/lib/map_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class mapScreen extends StatefulWidget {
+  const mapScreen({super.key});
+
+  @override
+  State<mapScreen> createState() => _mapScreenState();
+}
+
+class _mapScreenState extends State<mapScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
